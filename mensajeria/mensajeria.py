74e4e363f116adcc144ba4dbd159cd8080e8bcb3
Alan Velasco <AlanVelasco.A@gmail.com>
2018-05-02 12:19:18 -0500

    Change things so Heroku works

--- mensajeria/mensajeria.py
+++ mensajeria/mensajeria.py
@@ -1,5 +1,21 @@
-from flask import Flask, render_template
-from db import get_db, close_db
+# create and configure the app
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_mapping(
+    SECRET_KEY="dev", DATABASE=os.path.join(app.instance_path, "mensajeria.sqlite")
+)
+test_config = None
+if test_config is None:
+    # load the instance config, if it exists, when not testing
+    app.config.from_pyfile("config.py", silent=True)
+else:
+    # load the test config if passed in
+    app.config.from_mapping(test_config)
+
+# ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
 
 
 @app.route("/", methods=["GET"])
@@ -10,35 +26,48 @@ def inicio():
 @app.route("/enviar", methods=["GET", "POST"])
 def envio():
     if request.method == "POST":
-        sender = request.form["uEnviador"]
-        receiver = request.form["uRecibidor"]
-        message = request.form["uContenido"]
-        database = get_db()
-        database.execute(
-            "INSERT INTO messages (sender, receiver, content) VALUES (?, ?, ?)",
-            (sender, receiver, message),
-        )
-        database.commit()
+        data = request.get_json(force=True, silent=True)
+        sender = data["uEnviador"]
+        receiver = data["uRecibidor"]
+        message = data["uContenido"]
+        response = {"status": "success"}
+        try:
+            database = db.get_db()
+            database.execute(
+                "INSERT INTO messages (sender, receiver, content) VALUES (?, ?, ?)",
+                (sender, receiver, message),
+            )
+            database.commit()
+        except:
+            raise
 
-    return render_template("new_message.html")
+            response["status"] = "error"
+        return jsonify(response)
+
+    else:
+        return render_template("new_message.html")
 
 
 @app.route("/leer", methods=["GET", "POST"])
 def leer():
     if request.method == "POST":
-        user = request.form['user_name']
-        database = get_db()
+        user = request.form["user_name"]
+        print(user)
+        database = db.get_db()
         messages = database.execute(
-            "SELECT FROM messages (sender, content) WHERE receiver = ?",
-            user
+            "SELECT sender, content FROM messages WHERE receiver = ?", (user,)
         ).fetchall()
-        if messages is None:
-            render_template('inbox_select.html', vacio=True)
-        else:
-            render_template('inbox.html', user=user, messages=messages)
-    else:
-        render_template('inbox_select.html', vacio=False)
+        if messages:
+            return render_template("inbox.html", user=user, messages=messages)
 
+        else:
+            return render_template("inbox_select.html", vacio=True)
+
+    else:
+        return render_template("inbox_select.html", vacio=False)
+
+
+db.init_app(app)
 
 if __name__ == "__main__":
     app.run()
